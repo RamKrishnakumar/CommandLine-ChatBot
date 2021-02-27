2eeefff325419417c030b0ff54f84f8c24426d48
Ram Krishna <ram.krishana36@gmail.com>
2021-02-27 22:42:29 +0530

    training

--- training.py
+++ training.py
@@ -4,13 +4,17 @@ import pickle
 import numpy as np
 
 import nltk
-from nltk.stem.import WordNetLemmatizer
+nltk.download('punkt')
+nltk.download('wordnet')
+# Lemmatisation in linguistics is the process of grouping together 
+# the inflected forms of a word so they can be analysed as a single item.
+from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
-from tesorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 
@@ -19,12 +23,58 @@ classes = []
 documents = []
 ignore_letters = ['?',',','!','.']
 
-for inten in intents['intents']:
+for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
-        words.append(word_list)
-        documents.append((word_list), intent['tag'])
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
 
-            print (documents)
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl','wb'))
+pickle.dump(words, open('classes.pkl','wb'))
+
+training = []
+output_empty=[0] * len(classes)
+
+for document in documents:
+    bag=[]
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+        output_row = list( output_empty)
+        output_row[classes.index(document[1])] = 1
+        training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+# 'relu' is rectified linera unit activation function for deep neural network
+#  it find applications in computer vision and speech recognition using deep
+# neural nets and computational neuroscience
+model.add(Dense(128,input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+# 'softmax' is a mathematical function that converts a vector of numbers into a vector of probabilities, 
+#  where the probabilities of each value are propotional to the relative scale of each value in vector.
+model.add(Dense(len(train_y[0]),activation='softmax'))
+
+
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Done")
